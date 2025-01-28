6bc353a28ae3c54cfa6a9d0edbb9bd7bb764b0ae
DrBooleani <notdeyvidsalvatoresemail@gmail.com>
2025-01-28 09:24:04 -0300

    feat: Slider Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { SliderModule } from './shared/slider/slider.module';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    SliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
